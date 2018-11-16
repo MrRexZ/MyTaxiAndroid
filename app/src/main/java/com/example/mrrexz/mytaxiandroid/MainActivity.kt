171e3700024ab54e9d59d70ff4863ffd07d6c290
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-17 00:19:46 +0700

    Registered custom application class to Manifest, added injection code to MainActivity

--- app/src/main/java/com/example/mrrexz/mytaxiandroid/MainActivity.kt
+++ app/src/main/java/com/example/mrrexz/mytaxiandroid/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mrrexz.mytaxiandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import dagger.android.AndroidInjection
+import dagger.android.support.AndroidSupportInjection
+import dagger.android.support.AndroidSupportInjectionModule
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
--- app/src/main/java/com/example/mrrexz/mytaxiandroid/base/MyTaxiApp.kt
+++ app/src/main/java/com/example/mrrexz/mytaxiandroid/base/MyTaxiApp.kt
@@ -2,6 +2,10 @@ package com.example.mrrexz.mytaxiandroid.base
 
 import android.app.Activity
 import android.app.Application
+import com.example.mrrexz.mytaxiandroid.di.component.AppComponent
+import com.example.mrrexz.mytaxiandroid.di.component.DaggerAppComponent
+import com.example.mrrexz.mytaxiandroid.di.module.ContextModule
+import com.example.mrrexz.mytaxiandroid.di.module.NetworkModule
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
@@ -13,13 +17,15 @@ class MyTaxiApp : Application(), HasActivityInjector {
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
-
+    lateinit var appComponent: AppComponent
     override fun activityInjector(): AndroidInjector<Activity> {
         return dispatchingAndroidInjector;
     }
 
     override fun onCreate() {
         super.onCreate()
+        appComponent = DaggerAppComponent.builder().baseApp(this).contextModule(ContextModule).netModule(NetworkModule).build()
+        appComponent.inject(this)
     }
 
 
